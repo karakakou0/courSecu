5edc771caf5444676f7916ae4343c6512ea0fc9c
dgrosjea <djalil.grosjean@capgemini.com>
2019-05-10 11:06:31 +0200

    first commit

---
+++ src/JavaMVCControllers/HelloWorldController.java
@@ -0,0 +1,31 @@
+package JavaMVCControllers;
+
+import JavaMVCViews.*;
+import JavaMVCModels.*;
+
+public class HelloWorldController {
+
+    public void startApplication() {
+        // View the application's GUI
+        HelloWorldView view = new HelloWorldView();
+        view.setVisible(true);
+    }
+
+    public String getMessage() {
+        try {
+            HelloWorldModel model = new HelloWorldModel();
+            return model.getData();
+        } catch (Exception er) {
+            return "There was an error.";
+        }
+    }
+
+    public boolean writeMessage(String message) {
+        try {
+            HelloWorldModel model = new HelloWorldModel();
+            return model.writeData(message);
+        } catch (Exception er) {
+            return false;
+        }
+    }
+}
---
+++ src/javahellocollections/JavaHelloCollections.java
@@ -0,0 +1,11 @@
+package javahellocollections;
+
+import JavaMVCControllers.*;
+
+public class JavaHelloCollections {
+
+    public static void main(String[] args) {
+        HelloWorldController controller = new HelloWorldController();
+        controller.startApplication();
+    }
+}
